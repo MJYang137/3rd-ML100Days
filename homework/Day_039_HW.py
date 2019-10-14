c51ca119d46c2160c1165c26ce7138d1d00e088b
MJYang137 <mingjayyoung@gmail.com>
2019-10-14 21:30:17 +0800

    Day_39

---
+++ homework/Day_039_HW.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 14 21:26:25 2019
+
+@author: mingjay
+"""
+
+[作業重點]
+清楚了解 L1, L2 的意義與差異為何，並了解 LASSO 與 Ridge 之間的差異與使用情境
+
+作業
+請閱讀相關文獻，並回答下列問題
+
+脊回歸 (Ridge Regression) Linear, Ridge, Lasso Regression 本質區別
+
+LASSO 回歸可以被用來作為 Feature selection 的工具，請了解 LASSO 模型為什麼可用來作 Feature selection
+當自變數 (X) 存在高度共線性時，Ridge Regression 可以處理這樣的問題嗎? yes
