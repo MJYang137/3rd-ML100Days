22a805f85efd81a170b70ff7ec1f5eefcd0bf5b3
MJYang137 <mingjayyoung@gmail.com>
2019-10-14 21:29:50 +0800

    Day_37

---
+++ homework/Day_037_HW.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 14 21:25:52 2019
+
+@author: mingjay
+"""
+
+[作業重點]
+了解線性回歸的模型發展歷程，並了解優勢與劣勢，以及其使用情境
+
+作業
+請閱讀以下相關文獻，並回答以下問題
+
+Linear Regression 詳細介紹
+
+Logistics Regression 詳細介紹
+
+線性回歸模型能夠準確預測非線性關係的資料集嗎?
+回歸模型是否對資料分布有基本假設?
+No
+Yes
