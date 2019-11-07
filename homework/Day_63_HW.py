ceec4b9effbc503f475a90f84b4a90151bddcc74
MJYang137 <mingjayyoung@gmail.com>
2019-11-07 21:50:58 +0800

    Day_63

---
+++ homework/Day_63_HW.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  7 20:39:04 2019
+
+@author: mingjay
+"""
+
+#在精簡深度學習的方式上 : 卷積類神經 (CNN) 採用像素遠近，而遞歸類神經 (RNN) 採用著則是時間遠近
+#那麼，既然有著類似的設計精神，兩者是否有可能互換應用呢?
+
+
+#似乎也有人拿RNN來做圖像: 參見 
+#DRAW(Deep Recurrent Attentive Writer): A Recurrent Neural Network For Image Generation
